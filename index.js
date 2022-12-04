b559f3cf258049d9caeca708cf63ef3783fd48b6
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-12-04 03:35:55 -0500

    marketaux api call fixed using fetch

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const axios = require('axios')
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const{PORT,API_MARKETAUX,API_ALPHA_VANTAGE} = process.env;
+const cors = require('cors');
 
-app.listen(8080,()=>{
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT,()=>{
     console.log('app is litening to port 8080');
 })
+
+axios
+    .get('https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&language=en&limit=3&api_token=' + API_MARKETAUX)
+    .then(response=>{
+        console.log(response.data);
+    }).catch(error=>{
+        console.log(error);
+    });
+// fetch('https://api.marketaux.com/v1/news/all?exchanges=NYSE&filter_entities=true&language=en&limit=3&api_token=' + API_MARKETAUX).then(response=>response.json()).then(response=>console.log(response.data[0].uuid));
