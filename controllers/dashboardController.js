d14846a8ace728088e612d3bbf231be74e72a4fa
NickSigurdsson <nicholassigurdsson@gmail.com>
2023-01-10 20:33:41 -0500

    dashboard news feed customization logic done

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const knex = require("knex")(require("../knexfile"));
 require('dotenv').config();
+const url = require('url');
 const{API_MARKETAUX,API_ALPHA_VANTAGE} = process.env;
 
 exports.watchlist = async (req,res) => {
@@ -34,12 +35,10 @@ exports.portfolio = async (req,res) => {
             "id",
             "ticker",
         );
-
         const latestStockPrice = [];
         for (let i=0;i<portfolioData.length;i++){
             // this is how to grab the ticker for each item in the list
             const portfolioDataTicker = portfolioData[i].ticker
-            // // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MMAT&outputsize=compact&apikey=3NSM9679F4Z9LTNT
             const axiosResponse = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${portfolioDataTicker}&outputsize=compact&apikey=${API_ALPHA_VANTAGE}`);
             const axiosData = axiosResponse.data;
             const stockPriceList = Object.values(axiosData)[1];
@@ -55,7 +54,8 @@ exports.portfolio = async (req,res) => {
 
 exports.currentNews = async (req,res) => {
     // input from front end (endpoint used): http://localhost:8080/dashboard/news
-    const newsListParameter = 'symbols=MMAT';
+    const {ticker} = url.parse(req.url, true).query; 
+    const newsListParameter = `symbols=${ticker}`;
     try{
         const response = await axios.get(`https://api.marketaux.com/v1/news/all?${newsListParameter}&filter_entities=true&language=en&limit=3&api_token=${API_MARKETAUX}`);
         // SEND IS LIKE A RETURN, IF YOU DON'T DO IT IT IT WILL GIVE YOU AN ERROR
